c7f4dbd03f3f3358c29b3423f181121f5158b195
Lastlilith <Lastlilith.2014@gmail.com>
2023-01-03 18:22:41 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val koinKtor: String by project
+
+
+plugins {
+    application
+    kotlin("jvm") version "1.8.0"
+    id("io.ktor.plugin") version "2.2.1"
+                id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+}
+
+group = "com.imnidasoftware"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+
+    // Koin for Ktor
+    implementation ("io.insert-koin:koin-ktor:$koinKtor")
+    implementation ("io.insert-koin:koin-logger-slf4j:$koinKtor")
+
+    //Default Headers plugin
+    implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+}
---
+++ src/main/kotlin/com/imnidasoftware/Application.kt
@@ -0,0 +1,16 @@
+package com.imnidasoftware
+
+import io.ktor.server.application.*
+import com.imnidasoftware.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureKoin()
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+    configureDefaultHeader()
+}

48c8e642a5fb6b1aa03fe4bec73a3504dcfc956b
Lastlilith <Lastlilith.2014@gmail.com>
2023-01-04 11:50:06 +0900

    Created hero repository

---
+++ src/main/kotlin/com/imnidasoftware/repository/HeroRepository.kt
@@ -0,0 +1,17 @@
+package com.imnidasoftware.repository
+
+import com.imnidasoftware.models.ApiResponse
+import com.imnidasoftware.models.Hero
+
+interface HeroRepository {
+
+    val heroes: Map<Int, List<Hero>>
+
+    val page1: List<Hero>
+    val page2: List<Hero>
+    val page3: List<Hero>
+    val page4: List<Hero>
+    val page5: List<Hero>
+    suspend fun getAllHeroes(page: Int = 1): ApiResponse
+    suspend fun searchHeroes(name: String): ApiResponse
+}
